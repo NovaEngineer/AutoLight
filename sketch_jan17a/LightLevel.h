cbf04ee0bb090bdb30f47d19fb29013b0a48595f
NovaEngineer <mr.nova.diy@gmail.com>
2017-02-23 13:29:19 +0700

    Add files via upload

---
+++ sketch_jan17a/LightLevel.h
@@ -0,0 +1,6 @@
+#pragma once
+# ifndef _LIGHTLEVEL_H_
+#define _LIGHTLEVEL_H_
+void manualTune(int, int, int);
+void autoTune(int, int, int);
+#endif
---
+++ sketch_jan17a/Tune.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef _TUNE_H
+#define _TUNE_H
+void EEPROM_Res();
+uint32_t TuneLow();
+uint32_t TuneUp();
+void Tune();
+void TuneInform();
+#endif
